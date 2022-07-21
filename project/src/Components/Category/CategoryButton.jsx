0594072a71c0e24f6aba2a46b82e7ffc75caa148
vin9012d <vin9012c@gmail.com>
2022-07-21 11:13:41 +0530

    day2

--- project/src/Components/Category/CategoryButton.jsx
+++ project/src/Components/Category/CategoryButton.jsx
@@ -2,6 +2,6 @@ import { Button } from "@chakra-ui/react";
 
 export default function CategoryButton  ({children}) {
     return (
-        <Button sx={{ backgroundColor: "white",right:"50px", color: "yellow", width:"250px" , height:"50px", position: "absolute", bottom: "6%" ,  fontWeight:"bolder" }}>{children }</Button>
+        <Button sx={{ backgroundColor: "white",right:"50px", color: "yellow", width:"70%" , height:"50px", position: "absolute", bottom: "6%" ,  fontWeight:"bolder" }}>{children }</Button>
     )
 }
